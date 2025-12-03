ed2565f32d15d78da89a499c1fe13017ac5f4880
PALLA LAVANYA <163844326+pallalavanya22@users.noreply.github.com>
2025-12-03 15:43:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,231 +3,74 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.DEFAULT_EXTENSIONS = void 0;
-Object.defineProperty(exports, "File", {
-  enumerable: true,
-  get: function () {
-    return _file.default;
+exports.declare = declare;
+exports.declarePreset = void 0;
+const apiPolyfills = {
+  assertVersion: api => range => {
+    throwVersionError(range, api.version);
   }
-});
-Object.defineProperty(exports, "buildExternalHelpers", {
-  enumerable: true,
-  get: function () {
-    return _buildExternalHelpers.default;
-  }
-});
-Object.defineProperty(exports, "createConfigItem", {
-  enumerable: true,
-  get: function () {
-    return _index2.createConfigItem;
-  }
-});
-Object.defineProperty(exports, "createConfigItemAsync", {
-  enumerable: true,
-  get: function () {
-    return _index2.createConfigItemAsync;
-  }
-});
-Object.defineProperty(exports, "createConfigItemSync", {
-  enumerable: true,
-  get: function () {
-    return _index2.createConfigItemSync;
-  }
-});
-Object.defineProperty(exports, "getEnv", {
-  enumerable: true,
-  get: function () {
-    return _environment.getEnv;
-  }
-});
-Object.defineProperty(exports, "loadOptions", {
-  enumerable: true,
-  get: function () {
-    return _index2.loadOptions;
-  }
-});
-Object.defineProperty(exports, "loadOptionsAsync", {
-  enumerable: true,
-  get: function () {
-    return _index2.loadOptionsAsync;
-  }
-});
-Object.defineProperty(exports, "loadOptionsSync", {
-  enumerable: true,
-  get: function () {
-    return _index2.loadOptionsSync;
-  }
-});
-Object.defineProperty(exports, "loadPartialConfig", {
-  enumerable: true,
-  get: function () {
-    return _index2.loadPartialConfig;
-  }
-});
-Object.defineProperty(exports, "loadPartialConfigAsync", {
-  enumerable: true,
-  get: function () {
-    return _index2.loadPartialConfigAsync;
-  }
-});
-Object.defineProperty(exports, "loadPartialConfigSync", {
-  enumerable: true,
-  get: function () {
-    return _index2.loadPartialConfigSync;
-  }
-});
-Object.defineProperty(exports, "parse", {
-  enumerable: true,
-  get: function () {
-    return _parse.parse;
-  }
-});
-Object.defineProperty(exports, "parseAsync", {
-  enumerable: true,
-  get: function () {
-    return _parse.parseAsync;
-  }
-});
-Object.defineProperty(exports, "parseSync", {
-  enumerable: true,
-  get: function () {
-    return _parse.parseSync;
-  }
-});
-exports.resolvePreset = exports.resolvePlugin = void 0;
-Object.defineProperty((0, exports), "template", {
-  enumerable: true,
-  get: function () {
-    return _template().default;
-  }
-});
-Object.defineProperty((0, exports), "tokTypes", {
-  enumerable: true,
-  get: function () {
-    return _parser().tokTypes;
-  }
-});
-Object.defineProperty(exports, "transform", {
-  enumerable: true,
-  get: function () {
-    return _transform.transform;
-  }
-});
-Object.defineProperty(exports, "transformAsync", {
-  enumerable: true,
-  get: function () {
-    return _transform.transformAsync;
-  }
-});
-Object.defineProperty(exports, "transformFile", {
-  enumerable: true,
-  get: function () {
-    return _transformFile.transformFile;
-  }
-});
-Object.defineProperty(exports, "transformFileAsync", {
-  enumerable: true,
-  get: function () {
-    return _transformFile.transformFileAsync;
-  }
-});
-Object.defineProperty(exports, "transformFileSync", {
-  enumerable: true,
-  get: function () {
-    return _transformFile.transformFileSync;
-  }
-});
-Object.defineProperty(exports, "transformFromAst", {
-  enumerable: true,
-  get: function () {
-    return _transformAst.transformFromAst;
-  }
-});
-Object.defineProperty(exports, "transformFromAstAsync", {
-  enumerable: true,
-  get: function () {
-    return _transformAst.transformFromAstAsync;
-  }
-});
-Object.defineProperty(exports, "transformFromAstSync", {
-  enumerable: true,
-  get: function () {
-    return _transformAst.transformFromAstSync;
-  }
-});
-Object.defineProperty(exports, "transformSync", {
-  enumerable: true,
-  get: function () {
-    return _transform.transformSync;
-  }
-});
-Object.defineProperty((0, exports), "traverse", {
-  enumerable: true,
-  get: function () {
-    return _traverse().default;
-  }
-});
-exports.version = exports.types = void 0;
-var _file = require("./transformation/file/file.js");
-var _buildExternalHelpers = require("./tools/build-external-helpers.js");
-var resolvers = require("./config/files/index.js");
-var _environment = require("./config/helpers/environment.js");
-function _types() {
-  const data = require("@babel/types");
-  _types = function () {
-    return data;
-  };
-  return data;
-}
-Object.defineProperty((0, exports), "types", {
-  enumerable: true,
-  get: function () {
-    return _types();
-  }
-});
-function _parser() {
-  const data = require("@babel/parser");
-  _parser = function () {
-    return data;
-  };
-  return data;
-}
-function _traverse() {
-  const data = require("@babel/traverse");
-  _traverse = function () {
-    return data;
-  };
-  return data;
-}
-function _template() {
-  const data = require("@babel/template");
-  _template = function () {
-    return data;
-  };
-  return data;
-}
-var _index2 = require("./config/index.js");
-var _transform = require("./transform.js");
-var _transformFile = require("./transform-file.js");
-var _transformAst = require("./transform-ast.js");
-var _parse = require("./parse.js");
-;
-const version = exports.version = "7.28.5";
-const resolvePlugin = (name, dirname) => resolvers.resolvePlugin(name, dirname, false).filepath;
-exports.resolvePlugin = resolvePlugin;
-const resolvePreset = (name, dirname) => resolvers.resolvePreset(name, dirname, false).filepath;
-exports.resolvePreset = resolvePreset;
-const DEFAULT_EXTENSIONS = exports.DEFAULT_EXTENSIONS = Object.freeze([".js", ".jsx", ".es6", ".es", ".mjs", ".cjs"]);
+};
 {
-  exports.OptionManager = class OptionManager {
-    init(opts) {
-      return (0, _index2.loadOptionsSync)(opts);
+  Object.assign(apiPolyfills, {
+    targets: () => () => {
+      return {};
+    },
+    assumption: () => () => {
+      return undefined;
+    },
+    addExternalDependency: () => () => {}
+  });
+}
+function declare(builder) {
+  return (api, options, dirname) => {
+    let clonedApi;
+    for (const name of Object.keys(apiPolyfills)) {
+      if (api[name]) continue;
+      clonedApi != null ? clonedApi : clonedApi = copyApiObject(api);
+      clonedApi[name] = apiPolyfills[name](clonedApi);
     }
-  };
-  exports.Plugin = function Plugin(alias) {
-    throw new Error(`The (${alias}) Babel 5 plugin is being run with an unsupported Babel version.`);
+    return builder(clonedApi != null ? clonedApi : api, options || {}, dirname);
   };
 }
-0 && (exports.types = exports.traverse = exports.tokTypes = exports.template = 0);
+const declarePreset = exports.declarePreset = declare;
+function copyApiObject(api) {
+  let proto = null;
+  if (typeof api.version === "string" && /^7\./.test(api.version)) {
+    proto = Object.getPrototypeOf(api);
+    if (proto && (!hasOwnProperty.call(proto, "version") || !hasOwnProperty.call(proto, "transform") || !hasOwnProperty.call(proto, "template") || !hasOwnProperty.call(proto, "types"))) {
+      proto = null;
+    }
+  }
+  return Object.assign({}, proto, api);
+}
+function throwVersionError(range, version) {
+  if (typeof range === "number") {
+    if (!Number.isInteger(range)) {
+      throw new Error("Expected string or integer value.");
+    }
+    range = `^${range}.0.0-0`;
+  }
+  if (typeof range !== "string") {
+    throw new Error("Expected string or integer value.");
+  }
+  const limit = Error.stackTraceLimit;
+  if (typeof limit === "number" && limit < 25) {
+    Error.stackTraceLimit = 25;
+  }
+  let err;
+  if (version.slice(0, 2) === "7.") {
+    err = new Error(`Requires Babel "^7.0.0-beta.41", but was loaded with "${version}". ` + `You'll need to update your @babel/core version.`);
+  } else {
+    err = new Error(`Requires Babel "${range}", but was loaded with "${version}". ` + `If you are sure you have a compatible version of @babel/core, ` + `it is likely that something in your build process is loading the ` + `wrong version. Inspect the stack trace of this error to look for ` + `the first entry that doesn't mention "@babel/core" or "babel-core" ` + `to see what is calling Babel.`);
+  }
+  if (typeof limit === "number") {
+    Error.stackTraceLimit = limit;
+  }
+  throw Object.assign(err, {
+    code: "BABEL_VERSION_UNSUPPORTED",
+    version,
+    range
+  });
+}
 
 //# sourceMappingURL=index.js.map
